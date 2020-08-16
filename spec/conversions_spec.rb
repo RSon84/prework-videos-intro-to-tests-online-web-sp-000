ec60515aed97fefb1a7d7c8471239c3a146a124b
Devin Wilson <craigvin84@yahoo.com>
2020-08-16 20:25:05 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -3,7 +3,7 @@ require_relative '../conversions.rb'
 describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
-      
+
       expect(grams).to eq(0.0)
     end
   end
